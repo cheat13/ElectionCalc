6b9619985bb74ed333779f087fd90c48d8ce063a
Kritsana <tragoolphrom@gmail.com>
2019-04-18 15:07:22 +0700

    implement DataChart

---
+++ ElectionCalc.Api/Models/DataChart.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace ElectionCalc.Api.Models
+{
+    public class DataChart
+    {
+        public List<DataSet> Datasets { get; set; }
+    }
+
+    public class DataSet
+    {
+        public string Label { get; set; }
+        public List<Data> Data { get; set; }
+        public string BackgroundColor { get; set; }
+    }
+
+    public class Data
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
